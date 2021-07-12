36eb732f3715a37b43f781932a7ae46184c0f06b
Bryan-Elam <elam.bryan@gmail.com>
2021-07-12 03:31:58 -0400

    media query

--- js/index.js
+++ js/index.js
@@ -1,5 +1,14 @@
 const navToggle = document.querySelector('.nav-toggle');
+const navLink = document.querySelectorAll('.nav_link');
 
+//spins hamburger to x and sets menu translateX to 0 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-x');
 });
+
+//menu removed at link click
+navLink.forEach(link => {
+    link.addEventListener('click', () => {
+         document.body.classList.remove('nav-x');   
+    })
+})
